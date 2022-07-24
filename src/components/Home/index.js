6ae71d2b81e95a030f24537e10d88f080168363f
Lyan Peña <34713514+Zevora@users.noreply.github.com>
2022-07-23 21:07:39 -0400

    Added more projects to the portfolio page. Fixed some animations

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -31,7 +31,7 @@ const Home = () => {
                 <img src={LogoTitle} alt="developer" />
                 <AnimatedLetters letterClass={letterClass} strArray={nameArray} index={15}/>    {/*using the animation to replace lobodan*/}
                 <br />
-                <AnimatedLetters letterClass={letterClass} strArray={jobArray} index={22}/>     {/*using the animation to replace web developer*/}
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} index={15}/>     {/*using the animation to replace web developer*/}
                 </h1>
                 <h2>Computer Scientist / Software Developer </h2>
                 <Link to="contact" className="flat-button">CONTACT ME</Link> 
