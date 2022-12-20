de767a2773a435697d8d87a1d8242889776ad79f
Lyan Peña <34713514+Zevora@users.noreply.github.com>
2022-12-20 15:43:38 -0500

    Updated website to support mobile layouts.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,43 +3,63 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-L.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser, faCode } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser, faCode, faBars, faClose } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt='logo' />
-            <img className='sub-logo' src={LogoSubtitle} alt='slobodan' />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to='/'>
-                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to='about'>
-                <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="portfolio-link" to='portfolio'>
-                <FontAwesomeIcon icon={faCode} color='#4d4d4e' />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to='contact'>
-                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
-            </NavLink>
-            
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/lyanp/'>
-                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel="noreferrer" href='https://github.com/Zevora/'>
-                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
-                </a>
-            </li>
-        </ul>
-    </div>
-)
+const Sidebar = () => {
+
+    const [showNav, setShowNav] = useState(false);
+
+    return (
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={LogoS} alt='logo' />
+                <img className='sub-logo' src={LogoSubtitle} alt='slobodan' />
+            </Link>
+            <nav className={showNav ? 'mobile-show' : ''}>
+                <NavLink exact="true" activeclassname="active" to='/' onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to='about' onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="portfolio-link" to='portfolio' onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faCode} color='#4d4d4e' />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to='contact' onClick={() => setShowNav(false)}>
+                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+                </NavLink>
+                <FontAwesomeIcon
+                    onClick={() => setShowNav(false)}
+                        icon={faClose}
+                        color='#ffd700'
+                        size='3x'
+                        className='hamburger-close-icon'
+                />
+
+            </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel="noreferrer" href='https://www.linkedin.com/in/lyanp/'>
+                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href='https://github.com/Zevora/'>
+                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                    </a>
+                </li>
+            </ul>
+            <FontAwesomeIcon
+                onClick={() => setShowNav(true)}
+                icon={faBars}
+                color='#ffd700'
+                size='3x'
+                className='hamburger-icon'
+            />
+        </div>
+    )
+}
 
 export default Sidebar
